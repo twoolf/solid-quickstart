02a9c10126e3b04d6d03c7bb767eb9282c14d8ef
Tom Woolf <twoolf@jhu.edu>
2022-12-29 19:23:22 +0000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,12 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Privacy Policy</Title>
+      <h1>Daily24</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        We respect your privacy. This privacy policy describes our collection of personal data when you use Daily24.
+        
       </p>
     </main>
   );
